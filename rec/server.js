b47e4c04e07c6e28c0869d814a5c392a4e484803
Samuel Cavalcanti Nunes Coronel <samcronwell@gmail.com>
2025-09-24 13:09:19 -0300

    Revert "projeto API-REST de tarefas"
    This reverts commit d875b2184361ddbd37a37b73ccda89a3187c4ac9.

--- rec/server.js
+++
@@ -1,7 +0,0 @@
-import 'dotenv/config' 
-import app from './app.js' 
-const port = process.env.PORT || 3000  
-
-app.listen(port, () => { 
-  console.log(`Servidor ON em http://localhost:${port}`) 
-})
